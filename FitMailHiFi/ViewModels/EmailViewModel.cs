4e7f60a0f41e461861d859db035c72c4add0fe75
ojanacek <janacek.o@gmail.com>
2014-10-16 14:44:23 +0200

    Improves UX when selecting recipients from contacts
    Clicking a contact automatically inverts its selection. Also when no
    contact is selected and the Confirm button is pressed a message will
    pop-up.

--- FitMailHiFi/ViewModels/EmailViewModel.cs
+++ FitMailHiFi/ViewModels/EmailViewModel.cs
@@ -7,13 +7,6 @@ namespace FitMailHiFi.ViewModels
     {
         public Email Email { get; set; }
 
-        private bool isChecked;
-        public bool IsChecked
-        {
-            get { return isChecked; }
-            set { SetField(ref isChecked, value); }
-        }
-
         public EmailViewModel(Email email)
         {
             Email = email;
--- FitMailHiFi/Views/PickContactsWindow.xaml.cs
+++ FitMailHiFi/Views/PickContactsWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using FitMailHiFi.ViewModels;
 
 namespace FitMailHiFi.Views
 {
@@ -11,6 +15,13 @@ namespace FitMailHiFi.Views
 
         private void ConfirmClicked(object sender, RoutedEventArgs e)
         {
+            var context = DataContext as MainViewModel;
+            if (context.Contacts.All(c => !c.IsSelected))
+            {
+                MessageBox.Show("Není vybrán žádný kontakt. Zaškrtněte prosím vybrané kotakty.");
+                return;
+            }
+
             DialogResult = true;
             Close();
         }
@@ -19,6 +30,13 @@ namespace FitMailHiFi.Views
         {
             DialogResult = false;
             Close();
+        }
+
+        private void ContactMouseClick(object sender, MouseButtonEventArgs e)
+        {
+            var grid = sender as Grid;
+            var selectedContact = grid.DataContext as ContactViewModel;
+            selectedContact.IsSelected = !selectedContact.IsSelected;
         }
     }
 }
